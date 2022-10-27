b00df3b8815bb08cb036bfdbabb9b826b4da9175
JULIO\USER <julio.zambrano2011@gmail.com>
2022-10-26 20:59:36 -0500

    Creacion de pantalla login

--- app/src/main/java/com/example/tuzu/MainActivity.kt
+++ app/src/main/java/com/example/tuzu/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.tuzu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tuzu.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btningresar.setOnClickListener{
+            Toast.makeText(this, "Ingreso", Toast.LENGTH_SHORT).show()
+            val login= Intent(this,login::class.java)
+            startActivity(login)
+
+        }
+        binding.btnregistro.setOnClickListener{
+
+            Toast.makeText(this, "Registro", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.btnsomos.setOnClickListener{
+            Toast.makeText(this, "Somos", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/tuzu/login.kt
+++ app/src/main/java/com/example/tuzu/login.kt
@@ -2,10 +2,49 @@ package com.example.tuzu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.tuzu.databinding.ActivityLoginBinding
+import java.util.regex.Pattern
+
 
 class login : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnIngresar.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.txtcontrasena.text.toString()
+
+           val jj =binding.txtcontrasena.error
+
+            when {
+                mPassword.isEmpty() || mEmail.isEmpty() -> {
+                    Toast.makeText(this, "Email o contraseña o incorrectos.",
+                        Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+
+
+                    if(!Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+                        Toast.makeText(this, "$jj +Ingrese un email valido.",
+                            Toast.LENGTH_SHORT).show()
+                    }   else {
+                        Toast.makeText(this, "Pasamos a validar con base de datos", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                }
+            }
+
+
+
+        }
+
+
+
     }
 }
