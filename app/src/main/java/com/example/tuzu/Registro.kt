e0a8040bd78f66a8fee556b226d90e163a6d3222
JULIO\USER <julio.zambrano2011@gmail.com>
2022-10-27 21:32:21 -0500

    Validacion formluario resgistro

--- app/src/main/java/com/example/tuzu/Registro.kt
+++ app/src/main/java/com/example/tuzu/Registro.kt
@@ -1,11 +1,102 @@
 package com.example.tuzu
 
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.core.graphics.red
+import com.example.tuzu.databinding.ActivityLoginBinding
+import com.example.tuzu.databinding.ActivityRegistroBinding
+import java.util.*
+import java.util.regex.Pattern
 
 class Registro : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistroBinding
+    lateinit var datePickerDialog: DatePickerDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnRegresar.setOnClickListener{
+
+            val login= Intent(this,login::class.java)
+            startActivity(login)
+        }
+
+        binding.txtFechaNacimiento.setOnClickListener {
+            val c: Calendar = Calendar.getInstance()
+            val mYear: Int = c.get(Calendar.YEAR)
+
+            val mMonth: Int = c.get(Calendar.MONTH)
+
+            val mDay: Int = c.get(Calendar.DAY_OF_MONTH)
+
+            datePickerDialog = DatePickerDialog(
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    binding.txtFechaNacimiento.setText(
+                        dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year
+                    )
+                }, mYear, mMonth, mDay
+            )
+            datePickerDialog.show()
+
+        }
+
+        binding.btnRegistrar.setOnClickListener{
+
+            val mNombres = binding.txtNombres.text.toString()
+            val mApellidos = binding.txtApellidos.text.toString()
+            val mEmail = binding.txtEmail.text.toString()
+            val mTelefono = binding.txtTelefono.text.toString()
+            val mPassword = binding.txtContrasena.text.toString()
+            val mFechanacimiento = binding.txtFechaNacimiento.text.toString()
+            val mRepeatPassword = binding.txtVeriContrasena.text.toString()
+
+            val passwordRegex = Pattern.compile("^" +
+                    "(?=.*[-@#$%^&+=])" +     // Al menos 1 carácter especial
+                    ".{6,}" +                // Al menos 4 caracteres
+                    "$")
+
+            val letras = Pattern.compile("^"+"(?=.*[qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNM])"+".{1,}"+"$")
+
+            if(mEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+                //binding.txtEmail.setBackgroundColor(Color.RED)
+                Toast.makeText(this, "Ingrese un email valido.",
+                    Toast.LENGTH_SHORT).show()
+            } else if (mNombres.isEmpty() || !letras.matcher(mNombres).matches()){
+                Toast.makeText(this, "Los nombres no deben tener caracteres numericos.",
+                    Toast.LENGTH_SHORT).show()
+            }else if (mApellidos.isEmpty() || !letras.matcher(mApellidos).matches()){
+                Toast.makeText(this, "Los apellidos no deben tener caracteres numericos.",
+                    Toast.LENGTH_SHORT).show()
+            }else if (mTelefono.isEmpty() || mTelefono.length<10){
+                Toast.makeText(this, "El numero celular esde minimo 10 caracteres.",
+                    Toast.LENGTH_SHORT).show()
+            }else if (mFechanacimiento.isEmpty()){
+                Toast.makeText(this, "El campo fecha es obligatorio.",
+                    Toast.LENGTH_SHORT).show()
+            }else if (mPassword.isEmpty() || !passwordRegex.matcher(mPassword).matches()){
+                Toast.makeText(this, "La contraseña es debil.",
+                    Toast.LENGTH_SHORT).show()
+            } else if (mPassword != mRepeatPassword){
+                Toast.makeText(this, "La contraseñas no coinciden.",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Aja todo ok para acceder", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/tuzu/login.kt
+++ app/src/main/java/com/example/tuzu/login.kt
@@ -1,15 +1,22 @@
 package com.example.tuzu
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.getBitmap
 import android.util.Patterns
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.tuzu.databinding.ActivityLoginBinding
 import java.util.regex.Pattern
 
 
+
 class login : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +66,46 @@ class login : AppCompatActivity() {
 
         }
 
+        binding.btnFoto.setOnClickListener{
+
+            Toast.makeText(this, "foto", Toast.LENGTH_SHORT).show()
+            dispatchTakePictureIntent()
+
+        }
+
+
+
+
 
 
     }
+
+
+
+
+
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView5.setImageBitmap(imageBitmap)
+        }
+    }
+
+
+
+
+
+
 }
